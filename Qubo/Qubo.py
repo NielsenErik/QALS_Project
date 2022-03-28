66ceba6277da1260482d76f6d68d02e3055be675
Erik Nielsen <eriknielsen98@gmail.com>
2022-03-28 13:18:21 +0200

    change corr with spermanr

--- Qubo/Qubo.py
+++ Qubo/Qubo.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3
 from sqlalchemy import false
+from scipy import stats
 from Data_Rescaler import rescaledDataframe
 from Data_Rescaler import vector_V
 from Data_Rescaler import german_credit_data
@@ -12,18 +13,24 @@ def column_Correlation(inputData, v_Vector):
         the paper say tha for simplicity in convinient to use peason correlation
         Pearson correlation '''
     rows, columns = inputData.shape
+    '''Initialize correlation column matrix and correlation vector'''
     corrColumnsMatrix = np.zeros((columns,columns))
     corrColumnsV = np.zeros(columns)
+    '''Calculate correlation between each column and also between quality vector'''
     v = np.asarray(v_Vector.astype(float))    
     for i in range(columns):
         x = np.asarray(inputData[:,i].astype(float))
-        tmpVect= np.corrcoef(x,v)
-        corrColumnsV[i] = tmpVect[0,1]     
+        tmpVect, tmp= stats.spearmanr(x,v)
+        corrColumnsV[i] = tmpVect    
         for j in range(columns):            
             y = np.asarray(inputData[:,j].astype(float))
-            tmpMatrix = np.corrcoef(x, y)               
-            corrColumnsMatrix[i,j] = tmpMatrix[1,0]
+            tmpMatrix , tmp = stats.spearmanr(x,y)  
+            corrColumnsMatrix[i,j] = tmpMatrix     
     print(corrColumnsMatrix, " ", corrColumnsMatrix.shape)
     print(corrColumnsV)
     return corrColumnsV, corrColumnsMatrix
 '''Done ro column i column j and ro column j e vector v'''
+data = german_credit_data()
+dataMatrix = rescaledDataframe(data)
+v = vector_V(data)
+column_Correlation(dataMatrix, v)
