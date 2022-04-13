87b632e313db053ab6225e873b52a17a8f94855c
Erik Nielsen <eriknielsen98@gmail.com>
2022-04-13 17:58:00 +0200

    add input simulation

--- start.py
+++ start.py
@@ -6,25 +6,39 @@ import dwave_networkx as dnx
 import networkx as nx
 import neal
 import time
+import sys
+
+from sympy import ask
 from Qubo.german_credit_data import german_credit_data
 from Qubo.preprocessing_data import rescaledDataframe, vector_V
-
 
 from Qubo.solverQubo import QUBOsolver
 from Qubo.solverRFECV import RFECV_solver
 from Qubo.getAccuracyScore import getAccuracy
-from Qubo.graphs_for_dwave import generate_pegasus, get_Nodes, get_Q
 
+def ask_for_simulation():
+    print("Would you like to trya a simulation or run Dwave?")
+    simulation = input("[y] for simulation [n] for dwave: ")
+    sim = True
+    if(simulation == 'y'):
+        sim = True
+    elif(simulation == 'n'):
+        sim = False
+    else:
+        print("Wrong answer, terminating program...")
+        exit()
+    return sim
 
 def main():
     print("This program aim is optimal feature selection") 
     print("in credit scoring using quantum annealer")
+    sim = ask_for_simulation()
     data = german_credit_data()
     inputMatrix = rescaledDataframe(data)
     inputVector = vector_V(data)
     alpha = 0.977
     
-    qubo_array= QUBOsolver(48, alpha, inputMatrix, inputVector, 1 , 10 ,simulation = False)
+    qubo_array= QUBOsolver(48, alpha, inputMatrix, inputVector, 1 , 10 ,simulation = sim)
     rfecv_array = RFECV_solver(inputMatrix, inputVector)
     scoreQubo, feature_nQ = getAccuracy(qubo_array, inputMatrix, inputVector, isQubo= True)
     scoreRfecv, feature_nR = getAccuracy(rfecv_array, inputMatrix, inputVector, isQubo= False)
